c3edc5683fe603555d35851cdf065b73ac50c192
Gowthamcs <111518850+Gowthamcs@users.noreply.github.com>
2022-09-18 12:09:31 +0530

    Create questions.js

---
+++ online quiz app-api/questions.js
@@ -0,0 +1,153 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: " HTML stands for ?",
+    answer: "HyperText Markup Language",
+    options: [
+      "HighText Machine Language",
+      "HyperText and links Markup Language",
+      "HyperText Markup Language",
+      "None of these"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "  The tags in HTML are -?",
+    answer: "not case sensitive",
+    options: [
+      "case-sensitive",
+      "not case sensitive",
+      "in upper case",
+      "in lowercase"
+    ]
+  },
+  {
+    numb: 4,
+    question: " The <hr> tag in HTML is used for - ?",
+    answer: "horizontal ruler",
+    options: ["new line", "vertical ruler", "new paragraph", "horizontal ruler"]
+  },
+  {
+    numb: 5,
+    question: "HTML uses ______ ?",
+    answer: "Fixed tags defined by the language",
+    options: [
+      "User-defined tags",
+      "Predefined tags",
+      "Fixed tags defined by the language",
+      "Tags for links only"
+    ]
+  },
+  {
+    numb: 6,
+    question: "A stricter type of HTML document is known as ______ ?",
+    answer: "XHTML",
+    options: ["DHTML", "XML", "HTML", "XHTML"]
+  },
+  {
+    numb: 7,
+    question: "To create HTML page, you need _____ ?",
+    answer: "Both [A] and [B]",
+    options: [
+      "Web browser",
+      "text editor",
+      "Both [A] and [B]",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: " <a> and </a> are the tags used for ______ ?",
+    answer: "Adding links to your page",
+    options: [
+      "Adding image",
+      "Aligning text",
+      "Audio-voiced text",
+      "Adding links to your page"
+    ]
+  },
+  {
+    numb: 9,
+    question: " The BODY tag is usually used after ______ ?",
+    answer: "HEAD tag",
+    options: ["HTML tag", "EM tag", "HEAD tag", "TITLE tag"]
+  },
+  {
+    numb: 10,
+    question: "What does the <br> tag add to your webpage ?",
+    answer: "Line break",
+    options: [
+      "Long break",
+      "Paragraph break",
+      "Line break",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 11,
+    question: " <P> is called___ ?",
+    answer: "paragraph tag",
+    options: ["paragraph tag", "container tag", "head tag", "None of the above"]
+  },
+  {
+    numb: 12,
+    question: "WYSIWYG stands for _______ ?",
+    answer: "What You See Is What You Get",
+    options: [
+      "What you See Is What You Gain",
+      "When You Start Is When You Go",
+      "None of the above",
+      "What You See Is What You Get"
+    ]
+  },
+  {
+    numb: 13,
+    question: "When is the content of a table shown ?",
+    answer: "After the table is loaded",
+    options: [
+      "Before the border loads",
+      "After the table is loaded",
+      "In pieces as it loads",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 14,
+    question: " Which tag is used to insert images into your web page ?",
+    answer: "img",
+    options: ["img", "scr", "im", "None of the above"]
+  },
+  {
+    numb: 15,
+    question: " Gif and jpg are the two main types of what ?",
+    answer: "images",
+    options: ["animated effects", "videos", "None of the above", "images"]
+  }
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 16,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
